99e2739674c2810229fa59bb340b9622e01dd7fc
ankitkumarpatel <ankitkumar.patel@dal.ca>
2019-02-06 13:23:39 -0800

    histogram of maintenance types created.  subtask 2 of task 1 completed

--- task1.py
+++ task1.py
@@ -9,8 +9,15 @@ def tuple_required(filename):
 		tup = (line[2],line[4],line[6],line[7])
 		print(tup)
     
+# A histogram of the different types of maintenance. [Result has to be a dictionary with key as Maintenance  and number of streets as values] ["MAINTENANCE"]
 
-
+def maintenance_histogram(filename):
+	maintenance_types = dict()
+	fin = open(filename,'r')
+	for line in fin:
+		line = line.split(",")
+		maintenance_types[line[12]]=maintenance_types.get(line[12],0)+1
+	return(maintenance_types)
 
 
 
@@ -29,5 +36,5 @@ fin = open('Street_Centrelines.csv','r')
 
 print(tuple_required('Street_Centrelines.csv'))
 
-
+print(maintenance_histogram('Street_Centrelines.csv'))
 
