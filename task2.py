d13ba231f6615c34f20295999d239a8815a602f5
ankitkumarpatel <ankitkumar.patel@dal.ca>
2019-02-06 19:01:22 -0800

    addedv output formatting code

--- task2.py
+++ task2.py
@@ -36,7 +36,13 @@ def busstops(street_data,busstop_data):
 
     return (count_arterial_access,count_local_non,count_minor_inacess)
 
+def output_formatting(busstop_data):
+    print('| Stop Number | Location | FCODE |')
+    print('| ------ | ------ |------ |')
+    for data in busstop_data[0:10]:
+        print("|{}|{}|{}|".format(data[3],data[6],data[10]))
 
 street_list = readingfile('Street_Centrelines.csv')
 busstop_list = readingfile('Bus_Stops.csv')
-print("Bus stop data ::\n\nAccessible and in a ARTERIAL road: %d \nNon-Standard and in a LOCAL STREET: %d \nInaccessible and in a MINOR COLLECTOR: %d"% busstops(street_list,busstop_list))
+print("Bus stop data ::\n\nAccessible and in a ARTERIAL road: %d \nNon-Standard and in a LOCAL STREET: %d \nInaccessible and in a MINOR COLLECTOR: %d\n\n"% busstops(street_list,busstop_list))
+output_formatting(busstop_list)
