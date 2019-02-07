e8d16b8798256bda53b7de426875bf58411ce13f
ankitkumarpatel <ankitkumar.patel@dal.ca>
2019-02-06 16:13:08 -0800

    Subtask 4 completed and with this Task 1 done completelygit add task1.py git add task1.py git add task1.py git add task1.py git add task1.py git add task1.py git add task1.py

--- task1.py
+++ task1.py
@@ -28,10 +28,28 @@ def unique_owners(filename):
 	lst=list(dict.fromkeys(lst))
 	return(lst)
 
+# Different types of Street classes ["ST_CLASS"] and a list of streets undereach class
+def street(filename):
+	street_class_dict = dict()
+	fin = open(filename, 'r')
+	for line in fin:
+		line = line.split(",")
+		if line[10] not in street_class_dict:
+			street_class_dict[line[10]] = [line[2]]
+		else:
+			street_class_dict[line[10]].append(line[2])
+	return (street_class_dict)
+
 fin = open('Street_Centrelines.csv','r')
+print("\nResult of subtask 1: ")
+print(tuple_required('Street_Centrelines.csv'))
 
-#print(tuple_required('Street_Centrelines.csv'))
 
-#print(maintenance_histogram('Street_Centrelines.csv'))
+print("\nResult of subtask 2: ")
+print(maintenance_histogram('Street_Centrelines.csv'))
 
+print("\nResult of subtask 3: ")
 print(unique_owners('Street_Centrelines.csv'))
+
+print("\nResult of subtask 4: ")
+print(street('Street_Centrelines.csv'))
